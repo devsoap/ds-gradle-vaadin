ebcb126d3072f88ef662c7a5f4cfa984dd1f321c
John Ahlroos <john@ahlroos.me>
2019-03-13 17:34:07 +0200

    Support folder hierarchy in static resources (#208)

--- src/main/groovy/com/devsoap/vaadinflow/util/WebJarHelper.groovy
+++ src/main/groovy/com/devsoap/vaadinflow/util/WebJarHelper.groovy
@@ -158,7 +158,6 @@ class WebJarHelper {
             conf.allDependencies.each { Dependency dependency ->
                 if (dependency instanceof ProjectDependency) {
                     Project dependantProject = dependency.dependencyProject
-                    println dependantProject.tasks
                     dependantProject.tasks.withType(Jar).each {
                         jarTasks << it
                     }
